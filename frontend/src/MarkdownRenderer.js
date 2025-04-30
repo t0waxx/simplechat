c84fb1f3da462c6fafa68153a2efd4504fedd6d4
t0waxx <t0waxx101@gmail.com>
2025-04-30 18:08:28 +0900

    MarkdownRendererコンポーネントのレンダラーを整理し、カスタムコンポーネントを使用してマークダウンの表示を改善

--- frontend/src/MarkdownRenderer.js
+++ frontend/src/MarkdownRenderer.js
@@ -4,35 +4,20 @@ import remarkGfm from 'remark-gfm';
 import rehypeHighlight from 'rehype-highlight';
 import 'highlight.js/styles/github.css';
 
-const renderers = {
-    p: ({ node, ...props }) => <p className="markdown-paragraph" {...props} />,
-    h1: ({ node, ...props }) => <h1 className="markdown-heading" {...props} />,
-    h2: ({ node, ...props }) => <h2 className="markdown-heading" {...props} />,
-    h3: ({ node, ...props }) => <h3 className="markdown-heading" {...props} />,
-    code: ({ node, inline, className, children, ...props }) => {
-        const match = /language-(\w+)/.exec(className || '');
-        return !inline && match ? (
-            <pre className="markdown-code-block">
-                <code className={className} {...props}>
-                    {children}
-                </code>
-            </pre>
-        ) : (
-            <code className="markdown-inline-code" {...props}>
-                {children}
-            </code>
-        );
-    },
-};
+import { CustomH1 } from './CustomH1';
+import { CustomImage } from './CustomImage';
 
 export const MarkdownRenderer = ({ content }) => {
-    return (
-        <ReactMarkdown
-            remarkPlugins={[remarkGfm]}
-            rehypePlugins={[rehypeHighlight]}
-            components={renderers}
-        >
-            {content}
-        </ReactMarkdown>
-    );
+  return (
+    <ReactMarkdown
+      remarkPlugins={[remarkGfm]}
+      rehypePlugins={[rehypeHighlight]}
+      components={{
+        h1: CustomH1,
+        img: CustomImage,
+      }}
+    >
+      {content}
+    </ReactMarkdown>
+  );
 };
